8607ea379ecdbb470dcfd85af18f4455b188d6d1
clellandk2 <122688898+clellandk2@users.noreply.github.com>
2025-05-14 16:08:58 -0400

    Create Problem005.py

---
+++ Project Euler/Problem005.py
@@ -0,0 +1,8 @@
+def main():
+    #This one's really just a calculator question. You just need to think about how many of each prime number you'll need to have all 20 numbers evenly divide.
+    #Ends up being a product of all numbers of the form p^n where p is prime and n in the larget value it can be such that p^n < 20. 
+
+    num = 16*9*5*7*11*13*17*19
+    print(num)
+
+main()
